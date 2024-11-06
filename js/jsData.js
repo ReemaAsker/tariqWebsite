8db60ebceb05ff346f25cb0aeba5e787a382cf54
Reema Asker <reema1822000@gmail.com>
2024-11-06 16:06:12 +0200

    all files

---
+++ js/jsData.js
@@ -0,0 +1,76 @@
+
+var jsCourseDes = [
+    {
+        title: 'أساسيات جافا سكريبت', targetAudience: "المبتدئين إلى المتعلمين المتوسطين", courseLength: " 6hr 30min",
+        numberOfLectures: 34,
+        requirements: "فهم أساسي لمفاهيم البرمجة",
+    },
+]
+var allSections = [
+    [
+
+
+        { "title": "ماذا سنتعلم في هذه الدورة؟", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "أساسيات تطوير الويب", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+
+        { "title": "مقدمة إلى الجافاسكريبت", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+
+
+    ],
+    [
+        { "title": "Setting up work environment - بيئة العمل", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Variables - المتغيرات", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Comments and Variables Conventions - تسمية المتغيرات والملاحظات", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Operators and Expressions - المُعامِلات والتعابير", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Control Flow - التحكم في مجرى البرنامج", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Arrays 1 - السلاسل والمجموعات", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Arrays 2 - السلاسل والمجموعات", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Looping - الحلقات", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Functions - الدالات", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Practice 1 - تدريب", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Practice 2 - تدريب", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Scope - مجال المتغيرات", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Objects - الكائنات", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Object Methods - دالات الكائن", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Objects Practice - تدريب على الكائنات", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Document Object Model (DOM)", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Document Object Model (DOM) ", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+
+    ]
+    ,
+    [
+        { "title": "(Project Setup) التحقق من بيئة العمل", "link": "https://www.youtube.com/embed/2EAV2cB3FWY", description: 'xxxxxxxxxx', },
+        { "title": "(Menu) القائمة الرئيسية", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "(Profile) الصورة الشخصية مع المقدمة", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "(Social Networks) روابط التواصل الإجتماعي", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "(Skills) المهارات الشخصية", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "(Animating Skills) المهارات الشخصية", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "(Experience) الخبرة", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "(Footer) ذيل الصفحة", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "(Projects 1) المشاريع", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "(Projects 2) المشاريع", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "(Responsive) دعم مختلف الشاشات", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "GitHub", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "Git", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" },
+        { "title": "(GitHub Pages) نشر الموقع على الويب", "link": "https://www.youtube.com/embed/2EAV2cB3FWY" }
+    ]
+
+]
+var jsPlaylist = [
+    {
+        title: 'المقدمة', description: 'An introduction to the course.',
+
+        sections: allSections[0]
+
+    },
+    {
+        title: 'أساسيات الجافاسكريبت', description: 'Learn about variables in programming.',
+
+        sections: allSections[1]
+    },
+    {
+        title: 'مشروع الموقع الالكتروني Project ', link: 'https://www.youtube.com/embed/XUreKLul1og', description: 'Understand basic arithmetic operations.',
+        sections: allSections[2]
+    },
+
+];
